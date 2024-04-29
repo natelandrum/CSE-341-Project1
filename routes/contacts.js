9f5f3fe4dee7e4409b298c3760968ae396aca7d7
natelandrum <nate178dawg@gmail.com>
2024-04-29 15:18:37 -0600

    Update swagger.json response desc

--- routes/contacts.js
+++ routes/contacts.js
@@ -12,14 +12,19 @@ router.get("/:id", controller.getContact
 
 router.post("/", controller.createContact
 // #swagger.description = "Create a new contact"
+// #swagger.responses[500] = { description: 'Failed to Create Contact' }
 );
 
 router.put("/:id", controller.updateContact
 // #swagger.description = "Update a contact by ID"
+// #swagger.responses[500] = { description: 'Failed to Update Contact' }
+
 );
 
 router.delete("/:id", controller.deleteContact
 // #swagger.description = "Delete a contact by ID"
+// #swagger.responses[500] = { description: 'Failed to Delete Contact' }
+
 );
 
 module.exports = router;
